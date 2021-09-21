fb2ae9e35c896334b770f0b26f536f7a86d84cde
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-09-21 19:42:47 -0400

    main

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,7 +6,12 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
