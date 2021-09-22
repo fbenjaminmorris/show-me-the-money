d9c8dec2e4ebddc8673e99d4a2ed645c70096418
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-09-21 22:24:24 -0400

    main

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -9,6 +9,22 @@ ProductTag.init(
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
     }
   },
   {
